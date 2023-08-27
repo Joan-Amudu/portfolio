e6d51ae896aa59d41c03878dc7a07ea8d25e30b6
Joan <joan.baare@gmail.com>
2023-08-27 19:56:25 +1000

    Add and style content

--- portfolio-app/src/app/page.js
+++ portfolio-app/src/app/page.js
@@ -1,31 +1,28 @@
-import Image from 'next/image'
 import styles from './page.module.css'
+import Link from "next/link"
+import Button from '@mui/material/Button';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.center}>
         <div>  
-          <h1>Joan Amudu</h1>    
-          <h4>Full-Stack Developer</h4>
-          <p>Hi, I'm Joan Amudu a full-stack developer
-            <br></br>based in Sydney Australia
-          </p>
-        </div>
-        <Image
-          className={styles.logo}
-          src="/home-image.png"
-          alt="Joan Amudu picture"
-          width={280}
-          height={240}
-          priority
-        />
+          <h1>Hi, I'm <span>Joan Amudu</span></h1>
+          <br></br>   
+          <h3>A Full-Stack Developer based in Sydney Australia</h3>
+          <br></br>      
+          <p><span>TECH STACK:</span> HTML | CSS | JavaScript | React</p>
+        </div>                   
       </div>
-      <div>
-        <p>TECH STACK | 
-
-        </p>
-      </div>
+      <div className={styles.center}>
+        <Link href="/projects">
+          <Button style={{color:'#525FE1'}}>Learn More</Button>
+        </Link>
+        <Link href="/contact">
+          <Button style={{color:'#525FE1'}}>Contact Me</Button>
+        </Link>
+      </div> 
+            
     </main>
   )
 }
